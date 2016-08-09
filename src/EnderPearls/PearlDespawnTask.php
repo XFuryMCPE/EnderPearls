87d730c22370867da86d902e4fb557cd738a994f
Fury <xfurymcpe@gmail.com>
2016-08-09 16:13:41 -0500

    Wall collision despawning added

--- src/EnderPearls/PearlDespawnTask.php
+++ src/EnderPearls/PearlDespawnTask.php
@@ -25,20 +25,6 @@ class PearlDespawnTask extends PluginTask{
 		parent::__construct($plugin);
 		$this->plugin = $plugin;
 	}
-
-	/*public function onRun($currentTick){
-		foreach($this->plugin->getServer()->getLevels() as $lvls){
-			foreach($lvls->getEntities() as $ent){
-				if($ent instanceof Item){
-					if(isset($this->plugin->pearlLog[$ent->getId()])){
-						if($ent->getLevel()->getBlockIdAt($ent->getX(),$ent->getY() - 1,$ent->getZ()) != 0){
-							$ent->close();
-						}
-					}
-				}
-			}
-		}
-	}*/
 	
 	public function onRun($currentTick){
 		foreach($this->plugin->getServer()->getLevels() as $lvls){
@@ -46,10 +32,10 @@ class PearlDespawnTask extends PluginTask{
 				if($ent instanceof Item){
 					if(isset($this->plugin->pearlLog[$ent->getId()])){
 						$b_below = $ent->getLevel()->getBlockIdAt($ent->getX(),$ent->getY() - 1,$ent->getZ());
-						$b_side1 = $ent->getLevel()->getBlockIdAt($ent->getX() + 1,$ent->getY(),$ent->getZ());
-						$b_side2 = $ent->getLevel()->getBlockIdAt($ent->getX() - 1,$ent->getY(),$ent->getZ());
-						$b_side3 = $ent->getLevel()->getBlockIdAt($ent->getX(),$ent->getY(),$ent->getZ() + 1);
-						$b_side4 = $ent->getLevel()->getBlockIdAt($ent->getX(),$ent->getY(),$ent->getZ() - 1);
+						$b_side1 = $ent->getLevel()->getBlockIdAt($ent->getX() + 0.1,$ent->getY(),$ent->getZ());
+						$b_side2 = $ent->getLevel()->getBlockIdAt($ent->getX() - 0.1,$ent->getY(),$ent->getZ());
+						$b_side3 = $ent->getLevel()->getBlockIdAt($ent->getX(),$ent->getY(),$ent->getZ() + 0.1);
+						$b_side4 = $ent->getLevel()->getBlockIdAt($ent->getX(),$ent->getY(),$ent->getZ() - 0.1);
 						if($b_below != 0 || $b_side1 != 0 || $b_side2 != 0 || $b_side3 != 0 || $b_side4 != 0){
 							$ent->close();
 						}
